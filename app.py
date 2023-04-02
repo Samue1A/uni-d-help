12f3626e1e7f468bce331303a2ae0a2956f73605
Samue1A <92115015+Samue1A@users.noreply.github.com>
2023-04-02 15:38:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -510,12 +510,12 @@ def ussearch():
         useStuff.append(majors)
     if all['deadline']:
         l=0
-        deadline = ScrapGoogle(uni, '+university+application+deadline+date', 2).split('All results')[-1].replace('-', '.').replace('›', '.').split('.')
+        deadline = ScrapGoogle(uni, '+university+application+deadline+date', 2).split('All results')[-1].split('Verbatim')[-1].replace('- ', '.').replace('›', '.').split('.')
         for sentence in deadline:
             for word in sentence.split(' '):
                 if isnum(word) and int(word.replace(',', '')) < 32:
                     useStuffHead.append("Deadline")
-                    useStuff.append(sentence.split('All results')[-1])
+                    useStuff.append(sentence)
                     l = 1
                     break
             if l ==1:
