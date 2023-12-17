4fbc75e4abb3f8efc4f3f8a5e5617648fac619a6
Samue1A <92115015+Samue1A@users.noreply.github.com>
2023-12-17 18:47:26 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -343,6 +343,7 @@ def uksearch():
         links.append(item[-1])
     
     if all['acceptance rate']:
+        
         txt, focus = ScrapGoogle(uni, '+uk+university+acceptance+rate')
         if not focus:
             txt = txt.split('All results')[-1]
@@ -519,7 +520,12 @@ def ussearch():
 
 
     if all['acceptance rate']:
-        txt, focus = ScrapGoogle(uni, '+university+us+acceptance+rate')
+        url = 'https://www.bing.com/search?q=' + uni + '+university+us+acceptance+rate' + '&count=10'
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5023.114 Safari/537.36",
+        }
+        request_result = requests.get(url, headers=headers)
+        soup = bs4.BeautifulSoup(request_result.text, "html.parser")
         # if not focus:
         #     txt = txt.split('All results')[-1]
         #     for txt_index, item in enumerate(txt):
@@ -542,7 +548,7 @@ def ussearch():
         #     useStuffHead.append("Acceptance Rate")
         #     useStuff.append(f'The acceptance rate for {uni} is {txt}%')
         useStuffHead.append("Acceptance Rate")
-        useStuff.append(txt)
+        useStuff.append(soup)
             
 
 
